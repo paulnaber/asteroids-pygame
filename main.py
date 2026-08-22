6dbfed4a56e804a6beb535f7c414119285d676c3
Paul Naber <paul.naber@inside-m2m.de>
2026-08-22 10:13:53 +0200

    add event loop, fill black

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        # log all states
+        log_state()
+        # handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        # rerender
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
