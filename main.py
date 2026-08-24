fad462dfc2cb12df3feca71eebf9a456dcf12adb
Paul Naber <paul.naber@inside-m2m.de>
2026-08-24 18:24:32 +0200

    implement shots

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state, log_event
 from player import Player
+from shot import Shot
 import sys
 
 def main():
@@ -19,10 +20,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
 
     asteroid_field = AsteroidField()
 
